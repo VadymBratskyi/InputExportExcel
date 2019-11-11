0db3408b69f562eb6ba21d79269fff0476dd1fbc
Vadim Bratskyi <vadymbratskyi@gmail.com>
2019-11-11 09:42:36 +0200

    add files

---
+++ InpuExportExcel/InputExcel/Models/TestObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InputExcel.Models
+{
+    public class TestObject
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Number { get; set; }
+    }
+}
