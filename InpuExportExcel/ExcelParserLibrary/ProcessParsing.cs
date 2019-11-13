757d77c24d23301c0e462c191cc21c0294a57513
Vadim Bratskyi <vadymbratskyi@gmail.com>
2019-11-14 00:44:42 +0200

    openXml

---
+++ InpuExportExcel/ExcelParserLibrary/ProcessParsing.cs
@@ -0,0 +1,23 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using System;
+
+namespace ExcelParserLibrary
+{
+    public class ProcessParsing
+    {
+        public static void ReadExcelFile(string filePath) {
+
+            try {
+
+                using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filePath, false))
+                {
+
+                }
+
+            }
+            catch (Exception ex) {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
